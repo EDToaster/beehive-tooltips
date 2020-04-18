81210349858332bfded1f00ba29c25350e8e250b
EDToaster <elongateddanishtoaster@gmail.com>
2020-04-17 23:59:13 -0700

    Fix bug, release 1.1.1

--- src/main/java/ca/edtoaster/Utils.java
+++ src/main/java/ca/edtoaster/Utils.java
@@ -1,5 +1,9 @@
 package ca.edtoaster;
 
+import net.minecraft.block.BeehiveBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -8,32 +12,66 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.util.Optional;
+
 public class Utils {
+    public static class BeeData {
+        public final int numBees;
+        public final String honeyLevel;
+
+        public BeeData(int numBees, String honeyLevel) {
+            this.numBees = numBees;
+            this.honeyLevel = honeyLevel;
+        }
+    }
+
+    public static boolean isBeehive(ItemStack stack) {
+        Item i = stack.getItem();
+        if (!(i instanceof BlockItem)) return false;
+        BlockItem blockItem = (BlockItem) i;
+        Block block = blockItem.getBlock();
+        return block instanceof BeehiveBlock;
+    }
+
     /**
-     * Extracts the number of bees in the itemStack
+     * Extracts the data in the itemStack
      * Precondition: the itemstack is a Beehive blockItem
      *
      * @param stack the itemstack to display
-     * @return the number of Bees. If there are no nbt tags in the
+     * @return the beehive data. If there are no nbt tags in the
      * itemstack (For example, upon loading or getting the beehive from
-     * the creative menu), then this method will return 0.
+     * the creative menu), then this method will return no data.
      */
-    public static int extractNumberOfBees(ItemStack stack) {
+    public static Optional<BeeData> extractBeeData(ItemStack stack) {
         CompoundTag tag;
-        if ((tag = stack.getTag()) == null) return 0;
+        if ((tag = stack.getTag()) == null) return Optional.empty();
+
+        int numBees;
+        String honeyLevel;
 
         CompoundTag blockEntityTag = tag.getCompound("BlockEntityTag");
         ListTag beesTag = blockEntityTag.getList("Bees", 10);
-        return beesTag.size();
+        numBees = beesTag.size();
+
+        CompoundTag blockStateTag = tag.getCompound("BlockStateTag");
+        honeyLevel = blockStateTag.getString("honey_level");
+        System.out.println(blockStateTag);
+        return Optional.of(new BeeData(numBees, honeyLevel));
     }
 
     public static Text getBeeText(int numBees) {
-        return new LiteralText(numBees + " ")
-                .setStyle(WHITE_STYLE)
-                .append(BEES_TEXT);
+        return new LiteralText("Bees: ").setStyle(YELLOW_STYLE).append(new LiteralText(String.format("%d", numBees)).setStyle(WHITE_STYLE));
+    }
+
+    public static Text getHoneyText(String honeyLevel) {
+        return new LiteralText("Honey Level: ").setStyle(YELLOW_STYLE).append(new LiteralText(String.format("%s", honeyLevel)).setStyle(WHITE_STYLE));
+    }
+
+    public static Text getUnplacedText() {
+        return new LiteralText("Unplaced").setStyle(INVALID_STYLE);
     }
 
     public static final Style WHITE_STYLE = new Style().setColor(Formatting.WHITE);
     public static final Style YELLOW_STYLE = new Style().setColor(Formatting.YELLOW);
-    public static final Text BEES_TEXT = new LiteralText("Bees").setStyle(YELLOW_STYLE);
+    public static final Style INVALID_STYLE = new Style().setItalic(true).setColor(Formatting.GRAY);
 }
